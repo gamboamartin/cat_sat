9051165f5d5a42cf588977979966b95477ec9e03
mgamboa <gamboa.martin@gmail.com>
2022-06-29 17:25:15 -0500

    v0.33.2 select cat_sat_tipo_regimen

--- templates/inputs/cat_sat_tipo_persona/modifica.php
+++ templates/inputs/cat_sat_tipo_persona/modifica.php
@@ -1,10 +1,6 @@
+<?php /** @var  controllers\controlador_cat_sat_tipo_persona $controlador  Controlador en ejecucion */ ?>
 <?php use config\views; ?>
-<?php include (new views())->ruta_templates."inputs/modifica/id.php"; ?>
-<?php include (new views())->ruta_templates."inputs/modifica/codigo.php"; ?>
-<?php include (new views())->ruta_templates."inputs/modifica/codigo_bis.php"; ?>
-<?php include (new views())->ruta_templates."inputs/modifica/descripcion.php"; ?>
-<?php include (new views())->ruta_templates."inputs/modifica/alias.php"; ?>
-<?php include (new views())->ruta_templates."inputs/modifica/descripcion_select.php"; ?>
+<?php echo $controlador->forms_inputs_modifica; ?>
 <?php include "templates/botons/cat_sat_tipo_persona/valida_persona_fisica.php"; ?>
 <?php include (new views())->ruta_templates."botons/status.php"; ?>
 <?php include (new views())->ruta_templates.'botons/submit/modifica_bd.php';?>
