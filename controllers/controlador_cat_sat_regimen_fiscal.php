d1c2f554f0748eab22933a8c20430b144d2262db
mgamboa <gamboa.martin@gmail.com>
2022-06-30 14:24:14 -0500

    v0.34.2 Se genera link nuevo regimen fiscal

--- controllers/controlador_cat_sat_regimen_fiscal.php
+++ controllers/controlador_cat_sat_regimen_fiscal.php
@@ -8,9 +8,9 @@
  */
 namespace controllers;
 
-use gamboamartin\system\links_menu;
 use gamboamartin\system\system;
 use html\cat_sat_regimen_fiscal_html;
+use links\secciones\link_cat_sat_regimen_fiscal;
 use models\cat_sat_regimen_fiscal;
 use PDO;
 use stdClass;
@@ -20,14 +20,11 @@ class controlador_cat_sat_regimen_fiscal extends system {
     public function __construct(PDO $link, stdClass $paths_conf = new stdClass()){
         $modelo = new cat_sat_regimen_fiscal(link: $link);
         $html = new cat_sat_regimen_fiscal_html();
-        $obj_link = new links_menu($this->registro_id);
+        $obj_link = new link_cat_sat_regimen_fiscal($this->registro_id);
         parent::__construct(html:$html, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
 
         $this->titulo_lista = 'Regimenes Fiscales';
 
     }
-
-
-
 
 }
