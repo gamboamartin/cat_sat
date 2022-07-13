855e3f3de9ce675613342b4a582e8ced69e02e97
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-07-13 10:36:11 -0500

    v0.36.2 Se integra nuevso parametros para generacion de selects

--- templates/directivas/cat_sat_regimen_fiscal_html.php
+++ templates/directivas/cat_sat_regimen_fiscal_html.php
@@ -9,11 +9,11 @@ use PDO;
 
 class cat_sat_regimen_fiscal_html extends html_controler {
 
-    public function select_cat_sat_regimen_fiscal_id(int $id_selected, PDO $link): array|string
+    public function select_cat_sat_regimen_fiscal_id(int $cols, bool $con_registros, int $id_selected, PDO $link): array|string
     {
         $modelo = new cat_sat_regimen_fiscal($link);
 
-        $select = $this->select_catalogo(id_selected:$id_selected, modelo: $modelo);
+        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected, modelo: $modelo);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar select', data: $select);
         }
