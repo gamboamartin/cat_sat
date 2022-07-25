848d60ac4fccebfc1d1480eb3d176b7a9ca6c2d8
javier <javier.flores@ivitec.com.mx>
2022-07-25 14:25:51 -0500

    v0.39.3 Se agrego una funcion de directivas

--- templates/directivas/cat_sat_moneda_html.php
+++ templates/directivas/cat_sat_moneda_html.php
@@ -1,10 +1,22 @@
 <?php
 namespace html;
 
+use gamboamartin\errores\errores;
 use gamboamartin\system\html_controler;
+use models\cat_sat_moneda;
+use PDO;
 
 
 class cat_sat_moneda_html extends html_controler {
+    public function select_cat_sat_moneda_id(int $cols, bool $con_registros, int $id_selected, PDO $link): array|string
+    {
+        $modelo = new cat_sat_moneda($link);
 
+        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected, modelo: $modelo);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select', data: $select);
+        }
+        return $select;
+    }
 
 }
