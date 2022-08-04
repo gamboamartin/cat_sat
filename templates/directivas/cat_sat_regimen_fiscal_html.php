b980fa88c2e39c9b6973707a6a013ea3dbd3bb6f
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-04 11:32:25 -0500

    v.0.54.5 Se genera ut para select regimen fiscal

--- templates/directivas/cat_sat_regimen_fiscal_html.php
+++ templates/directivas/cat_sat_regimen_fiscal_html.php
@@ -3,14 +3,32 @@ namespace html;
 
 use gamboamartin\errores\errores;
 use gamboamartin\system\html_controler;
+use gamboamartin\template\directivas;
 use models\cat_sat_regimen_fiscal;
 use PDO;
 
 
 class cat_sat_regimen_fiscal_html extends html_controler {
 
+    /**
+     * Genera un select tipo regimen fiscal
+     * @param int $cols Numero de columnas css
+     * @param bool $con_registros si no con registros integra un select vacio
+     * @param int $id_selected identificador para selected
+     * @param PDO $link conexion a la base de datos
+     * @return array|string
+     * @version 0.54.5
+     * @verfuncion 0.1.0
+     * @fecha 2022-08-04 11:27
+     * @author mgamboa
+     */
     public function select_cat_sat_regimen_fiscal_id(int $cols, bool $con_registros, int $id_selected, PDO $link): array|string
     {
+        $valida = (new directivas(html:$this->html_base))->valida_cols(cols:$cols);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al validar cols', data: $valida);
+        }
+
         $modelo = new cat_sat_regimen_fiscal($link);
 
         $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected,
