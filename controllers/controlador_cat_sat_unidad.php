482b838927635bdbb2a50a29c9cef33427c9d172
Kevin <kevin.acuna@ivitec.com.mx>
2022-08-09 12:46:01 -0500

    v.0.55.5 Fix html_base en controlador unidad

--- controllers/controlador_cat_sat_unidad.php
+++ controllers/controlador_cat_sat_unidad.php
@@ -11,25 +11,22 @@ namespace controllers;
 use gamboamartin\system\links_menu;
 use gamboamartin\system\system;
 use html\cat_sat_unidad_html;
-use html\html;
+use gamboamartin\template\html;
 use models\cat_sat_unidad;
 use PDO;
 use stdClass;
 
 class controlador_cat_sat_unidad extends system {
 
-    public function __construct(PDO $link, stdClass $paths_conf = new stdClass()){
+    public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(),
+                                stdClass $paths_conf = new stdClass()){
+
         $modelo = new cat_sat_unidad(link: $link);
-        $html_base = new html();
-        $html = new cat_sat_unidad_html(html: $html_base);
+        $html_ = new cat_sat_unidad_html(html: $html);
         $obj_link = new links_menu($this->registro_id);
-        parent::__construct(html:$html, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
+        parent::__construct(html:$html_, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
 
         $this->titulo_lista = 'Unidades';
-
     }
-
-
-
 
 }
