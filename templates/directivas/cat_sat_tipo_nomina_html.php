cc91717cbcced2ed8d7bda75e3b38f8273853603
Kevin <kevin.acuna@ivitec.com.mx>
2022-08-16 12:25:31 -0500

    v.0.62.7 Se genero funcion select id para cat_sat_tipo_nomina

--- templates/directivas/cat_sat_tipo_nomina_html.php
+++ templates/directivas/cat_sat_tipo_nomina_html.php
@@ -4,9 +4,21 @@ namespace html;
 use gamboamartin\errores\errores;
 use gamboamartin\system\html_controler;
 use models\cat_sat_actividad_economica;
+use models\cat_sat_tipo_nomina;
 use PDO;
 
 class cat_sat_tipo_nomina_html extends html_controler {
 
+    public function select_cat_sat_tipo_nomina_id(int $cols,bool $con_registros,int $id_selected, PDO $link): array|string
+    {
+        $modelo = new cat_sat_tipo_nomina($link);
+
+        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected,
+            modelo: $modelo,label: 'Tipo nomina',required: true);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select', data: $select);
+        }
+        return $select;
+    }
 
 }
