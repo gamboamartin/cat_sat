7922091406d143d35468054fefd95bd2e50dd79e
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-30 13:39:48 -0500

    v.0.71.8 Se genera ut tipo nomina

--- templates/directivas/cat_sat_tipo_nomina_html.php
+++ templates/directivas/cat_sat_tipo_nomina_html.php
@@ -10,7 +10,17 @@ use PDO;
 
 class cat_sat_tipo_nomina_html extends html_controler {
 
-    public function select_cat_sat_tipo_nomina_id(int $cols,bool $con_registros,int|null $id_selected, PDO $link): array|string
+    /**
+     * Genera un select de tipo nomina
+     * @param int $cols No de columnas css
+     * @param bool $con_registros Si con registros asigna options
+     * @param int|null $id_selected Identificador para selected
+     * @param PDO $link Conexion a la BD
+     * @return array|string
+     * @version 0.71.8
+     */
+    public function select_cat_sat_tipo_nomina_id(int $cols,bool $con_registros,int|null $id_selected,
+                                                  PDO $link): array|string
     {
         $valida = (new directivas(html:$this->html_base))->valida_cols(cols:$cols);
         if(errores::$error){
