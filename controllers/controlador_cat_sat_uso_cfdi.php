421236bdfbd36d3dbd381b489e1591d75da7ec9d
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-08-30 11:23:31 -0500

    v.0.69.8 Se integra html entidad uso_cfdi

--- controllers/controlador_cat_sat_uso_cfdi.php
+++ controllers/controlador_cat_sat_uso_cfdi.php
@@ -10,20 +10,20 @@ namespace controllers;
 
 use gamboamartin\system\links_menu;
 use gamboamartin\system\system;
+use gamboamartin\template\html;
 use html\cat_sat_uso_cfdi_html;
-use html\html;
 use models\cat_sat_uso_cfdi;
 use PDO;
 use stdClass;
 
 class controlador_cat_sat_uso_cfdi extends system {
 
-    public function __construct(PDO $link, stdClass $paths_conf = new stdClass()){
+    public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(),
+                                stdClass $paths_conf = new stdClass()){
         $modelo = new cat_sat_uso_cfdi(link: $link);
-        $html_base = new html();
-        $html = new cat_sat_uso_cfdi_html(html: $html_base);
+        $html_ = new cat_sat_uso_cfdi_html(html: $html);
         $obj_link = new links_menu($this->registro_id);
-        parent::__construct(html:$html, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
+        parent::__construct(html: $html_, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
 
         $this->titulo_lista = 'Usos CFDI';
 
