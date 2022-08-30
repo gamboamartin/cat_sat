a8abee003cb1ecf7530180b8bcd502373f22d039
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-30 13:36:10 -0500

    v.0.70.8 Se ajusta param tipo nomina

--- templates/directivas/cat_sat_tipo_nomina_html.php
+++ templates/directivas/cat_sat_tipo_nomina_html.php
@@ -3,14 +3,24 @@ namespace html;
 
 use gamboamartin\errores\errores;
 use gamboamartin\system\html_controler;
+use gamboamartin\template\directivas;
 use models\cat_sat_actividad_economica;
 use models\cat_sat_tipo_nomina;
 use PDO;
 
 class cat_sat_tipo_nomina_html extends html_controler {
 
-    public function select_cat_sat_tipo_nomina_id(int $cols,bool $con_registros,int $id_selected, PDO $link): array|string
+    public function select_cat_sat_tipo_nomina_id(int $cols,bool $con_registros,int|null $id_selected, PDO $link): array|string
     {
+        $valida = (new directivas(html:$this->html_base))->valida_cols(cols:$cols);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al validar cols', data: $valida);
+        }
+
+        if(is_null($id_selected)){
+            $id_selected = -1;
+        }
+
         $modelo = new cat_sat_tipo_nomina($link);
 
         $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected,
