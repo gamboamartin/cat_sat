a766125af64f925cc9ac5d3f46e2cb5ad7d7e50a
mgamboa <gamboa.martin@gmail.com>
2022-09-27 13:34:29 -0500

    v.0.94.9 Se integra val mayus 1 char

--- orm/cat_sat_tipo_de_comprobante.php
+++ orm/cat_sat_tipo_de_comprobante.php
@@ -8,8 +8,9 @@ class cat_sat_tipo_de_comprobante extends modelo{
         $tabla = 'cat_sat_tipo_de_comprobante';
         $columnas = array($tabla=>false);
         $campos_obligatorios[] = 'descripcion';
+        $tipo_campos['codigo'] = 'cod_1_letras_mayusc';
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
-            columnas: $columnas);
+            columnas: $columnas, tipo_campos: $tipo_campos);
     }
 }
