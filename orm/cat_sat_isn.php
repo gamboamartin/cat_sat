01e354d6bc6eb11cf09fd4bed5198adfb67084d7
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-10-10 11:00:07 -0500

    v.0.98.9 Se agregan campos faltantes en alta isn

--- orm/cat_sat_isn.php
+++ orm/cat_sat_isn.php
@@ -1,6 +1,7 @@
 <?php
 namespace gamboamartin\cat_sat\models;
 use base\orm\modelo;
+use gamboamartin\direccion_postal\models\dp_estado;
 use PDO;
 
 class cat_sat_isn extends modelo{
@@ -9,7 +10,13 @@ class cat_sat_isn extends modelo{
         $columnas = array($tabla=>false);
         $campos_obligatorios[] = 'descripcion';
 
+        $campos_view = array();
+        $campos_view['dp_estado_id']['type'] = 'selects';
+        $campos_view['dp_estado_id']['model'] = (new dp_estado($link));
+
+        $campos_view['porcentaje']['type'] = 'inputs';
+
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
-            columnas: $columnas);
+            columnas: $columnas, campos_view: $campos_view);
     }
 }
--- templates/directivas/cat_sat_isn_html.php
+++ templates/directivas/cat_sat_isn_html.php
@@ -1,8 +1,36 @@
 <?php
 namespace html;
+use base\orm\modelo;
+use controllers\controlador_cat_sat_isn;
+use gamboamartin\errores\errores;
 use gamboamartin\system\html_controler;
+use PDO;
+use stdClass;
 
 
 class cat_sat_isn_html extends html_controler {
+    private function asigna_inputs_alta(controlador_cat_sat_isn $controler, array|stdClass $inputs): array|stdClass
+    {
+        $controler->inputs->select = new stdClass();
+        $controler->inputs->select->dp_estado_id = $inputs['selects']->dp_estado_id;
 
+        $controler->inputs->porcentaje = $inputs['inputs']->porcentaje;
+
+        return $controler->inputs;
+    }
+
+    public function genera_inputs_alta(controlador_cat_sat_isn $controler, modelo $modelo, PDO $link, array $keys_selects = array()): array|stdClass
+    {
+        $inputs = $this->init_alta2(row_upd: $controler->row_upd,modelo: $controler->modelo,link: $link,keys_selects:  $keys_selects);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar inputs',data:  $inputs);
+
+        }
+        $inputs_asignados = $this->asigna_inputs_alta(controler:$controler, inputs: $inputs);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al asignar inputs',data:  $inputs_asignados);
+        }
+
+        return $inputs_asignados;
+    }
 }
