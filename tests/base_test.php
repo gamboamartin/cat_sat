c3877888e2144d96727bae902d5c544a88caa8f6
mgamboa <gamboa.martin@gmail.com>
2022-10-11 11:54:33 -0500

    v.0.104.9 Se integra ut cat sat

--- tests/base_test.php
+++ tests/base_test.php
@@ -7,16 +7,22 @@ use gamboamartin\cat_sat\models\cat_sat_tipo_nomina;
 use gamboamartin\direccion_postal\models\dp_pais;
 use gamboamartin\errores\errores;
 
+use gamboamartin\test\test;
 use PDO;
+use stdClass;
 
 
 class base_test{
 
-    public function alta_cat_sat_moneda(PDO $link, int $id = 1, int $dp_pais_id = -1, bool $predeterminado = false): array|\stdClass
+    public function alta_cat_sat_moneda(PDO $link, string $codigo = 'XSM', string $descripcion = '1', int $id = 1,
+                                        int $dp_pais_id = -1, bool $predeterminado = false): array|stdClass
     {
-        $registro = array();
-        if($predeterminado){
-            $registro['predeterminado'] = 'activo';
+
+        $registro = (new test())->registro(codigo:$codigo,descripcion:  $descripcion, id: $id,
+            predeterminado:  $predeterminado);
+        if (errores::$error) {
+            return (new errores())->error('Error al integrar predeterminado si existe', $registro);
+
         }
 
         if($dp_pais_id === -1) {
@@ -32,6 +38,7 @@ class base_test{
                     return (new errores())->error('Error al eliminar', $del);
                 }
             }
+
             $existe = (new dp_pais($link))->existe_predeterminado();
             if (errores::$error) {
                 return (new errores())->error('Error al validar si existe', $existe);
@@ -50,6 +57,7 @@ class base_test{
         if($dp_pais_id > 0){
 
             $registro['dp_pais_id'] = $dp_pais_id;
+
             $existe = (new dp_pais($link))->existe_by_id(registro_id: $dp_pais_id);
             if (errores::$error) {
                 return (new errores())->error('Error al validar si existe', $existe);
@@ -64,9 +72,6 @@ class base_test{
 
         }
 
-        $registro['id'] = $id;
-        $registro['descripcion'] = 1;
-        $registro['codigo'] = 'XSM';
 
 
         $alta = (new cat_sat_moneda($link))->alta_registro($registro);
@@ -77,13 +82,16 @@ class base_test{
         return $alta;
     }
 
-    public function alta_cat_sat_regimen_fiscal(PDO $link): array|\stdClass
+    public function alta_cat_sat_regimen_fiscal(PDO $link, string $codigo = '1', string $descripcion = '1',
+                                                int $id = 1, bool $predeterminado = false): array|stdClass
     {
-        $registro = array();
-        $registro['id'] = 1;
-        $registro['codigo'] = 1;
-        $registro['descripcion'] = 1;
-        $registro['descripcion_select'] = 1;
+        $registro = (new test())->registro(
+            codigo:$codigo,descripcion: $descripcion,id: $id, predeterminado: $predeterminado);
+        if (errores::$error) {
+            return (new errores())->error('Error al integrar predeterminado si existe', $registro);
+
+        }
+
 
         $alta = (new cat_sat_regimen_fiscal($link))->alta_registro($registro);
         if(errores::$error){
@@ -93,15 +101,17 @@ class base_test{
         return $alta;
     }
 
-    public function alta_cat_sat_tipo_nomina(PDO $link): array|\stdClass
+    public function alta_cat_sat_tipo_nomina(PDO $link, string $codigo = '1', $descripcion = '1', int $id = 1,
+                                             bool $predeterminado = false): array|stdClass
     {
-        $nom_periodo = array();
-        $nom_periodo['id'] = 1;
-        $nom_periodo['codigo'] = 1;
-        $nom_periodo['descripcion'] = 1;
-        $nom_periodo['descripcion_select'] = 1;
+        $registro = (new test())->registro(
+            codigo:$codigo,descripcion: $descripcion,id: $id, predeterminado: $predeterminado);
+        if (errores::$error) {
+            return (new errores())->error('Error al integrar predeterminado si existe', $registro);
 
-        $alta = (new cat_sat_tipo_nomina($link))->alta_registro($nom_periodo);
+        }
+
+        $alta = (new cat_sat_tipo_nomina($link))->alta_registro($registro);
         if(errores::$error){
             return (new errores())->error(mensaje: 'Error al insertar', data: $alta);
 
