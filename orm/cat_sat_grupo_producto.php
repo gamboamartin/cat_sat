f94e34d4a17ff1f955de7f02638cec1bba785cc3
Kevin <kevin.acuna@ivitec.com.mx>
2022-11-11 18:33:30 -0500

    v0.134.12 Se ajusta grupo producto

--- orm/cat_sat_grupo_producto.php
+++ orm/cat_sat_grupo_producto.php
@@ -1,15 +1,70 @@
 <?php
 namespace gamboamartin\cat_sat\models;
 use base\orm\modelo;
+use gamboamartin\errores\errores;
 use PDO;
+use stdClass;
 
 class cat_sat_grupo_producto extends modelo{
     public function __construct(PDO $link){
         $tabla = 'cat_sat_grupo_producto';
-        $columnas = array($tabla=>false);
+        $columnas = array($tabla=>false,"cat_sat_division_producto" => $tabla,
+            "cat_sat_tipo_producto" => "cat_sat_division_producto");
         $campos_obligatorios[] = 'descripcion';
 
+        $campos_view['cat_sat_division_producto_id'] = array('type' => 'selects', 'model' => new cat_sat_division_producto($link));
+        $campos_view['codigo'] = array('type' => 'inputs');
+        $campos_view['descripcion'] = array('type' => 'inputs');
+
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
-            columnas: $columnas);
+            columnas: $columnas,campos_view: $campos_view);
+
+        $this->NAMESPACE = __NAMESPACE__;
+    }
+
+    public function alta_bd(): array|stdClass
+    {
+        $this->registro = $this->campos_base(data:$this->registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al inicializar campo base',data: $this->registro);
+        }
+
+        $r_alta_bd = parent::alta_bd();
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al insertar estado',data:  $r_alta_bd);
+        }
+        return $r_alta_bd;
+    }
+
+    private function campos_base(array $data): array
+    {
+        if(!isset($data['codigo_bis'])){
+            $data['codigo_bis'] =  $data['codigo'];
+        }
+
+        if(!isset($data['descripcion_select'])){
+            $ds = str_replace("_"," ",$data['descripcion']);
+            $ds = ucwords($ds);
+            $data['descripcion_select'] =  "{$data['codigo']} - {$ds}";
+        }
+
+        if(!isset($data['alias'])){
+            $data['alias'] = $data['codigo'];
+        }
+        return $data;
+    }
+
+    public function modifica_bd(array $registro, int $id, bool $reactiva = false): array|stdClass
+    {
+        $registro = $this->campos_base(data:$registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al inicializar campo base',data: $registro);
+        }
+
+        $r_modifica_bd = parent::modifica_bd($registro, $id, $reactiva);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al modificar estado',data:  $r_modifica_bd);
+        }
+        return $r_modifica_bd;
     }
 }
