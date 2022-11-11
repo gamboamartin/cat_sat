67b5cc92012ff9230e9cecc128be5cf53794e9f2
Kevin <kevin.acuna@ivitec.com.mx>
2022-11-11 18:08:30 -0500

    v0.133.12 Se ajusta division producto

---
+++ controllers/controlador_cat_sat_division_producto.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
+namespace controllers;
+
+use gamboamartin\cat_sat\models\cat_sat_division;
+use gamboamartin\cat_sat\models\cat_sat_division_producto;
+use gamboamartin\errores\errores;
+use gamboamartin\system\links_menu;
+use gamboamartin\system\system;
+use gamboamartin\template\html;
+use html\cat_sat_division_html;
+use html\cat_sat_division_producto_html;
+use PDO;
+use stdClass;
+
+class controlador_cat_sat_division_producto extends system {
+
+    public array $keys_selects = array();
+
+    public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(),
+                                stdClass $paths_conf = new stdClass()){
+        $modelo = new cat_sat_division_producto(link: $link);
+        $html_ = new cat_sat_division_producto_html(html: $html);
+        $obj_link = new links_menu(link: $link, registro_id: $this->registro_id);
+
+        $columns["cat_sat_division_producto_id"]["titulo"] = "Id";
+        $columns["cat_sat_division_producto_codigo"]["titulo"] = "Código";
+        $columns["cat_sat_division_producto_descripcion"]["titulo"] = "División";
+        $columns["cat_sat_tipo_producto_descripcion"]["titulo"] = "Tipo Producto";
+
+        $filtro = array("cat_sat_division_producto.id","cat_sat_division_producto.codigo","cat_sat_division_producto.descripcion",
+            "cat_sat_tipo_producto.descripcion");
+
+        $datatables = new stdClass();
+        $datatables->columns = $columns;
+        $datatables->filtro = $filtro;
+
+        parent::__construct(html:$html_, link: $link,modelo:  $modelo, obj_link: $obj_link, datatables: $datatables,
+            paths_conf: $paths_conf);
+
+        $this->titulo_lista = 'División';
+
+        $propiedades = $this->inicializa_priedades();
+        if(errores::$error){
+            $error = $this->errores->error(mensaje: 'Error al inicializar propiedades',data:  $propiedades);
+            print_r($error);
+            die('Error');
+        }
+    }
+
+    public function alta(bool $header, bool $ws = false): array|string
+    {
+        $r_alta =  parent::alta(header: false);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al generar template',data:  $r_alta, header: $header,ws:$ws);
+        }
+
+        $inputs = $this->genera_inputs(keys_selects:  $this->keys_selects);
+        if(errores::$error){
+            $error = $this->errores->error(mensaje: 'Error al generar inputs',data:  $inputs);
+            print_r($error);
+            die('Error');
+        }
+
+        return $r_alta;
+    }
+
+    public function asignar_propiedad(string $identificador, mixed $propiedades)
+    {
+        if (!array_key_exists($identificador,$this->keys_selects)){
+            $this->keys_selects[$identificador] = new stdClass();
+        }
+
+        foreach ($propiedades as $key => $value){
+            $this->keys_selects[$identificador]->$key = $value;
+        }
+    }
+
+    private function inicializa_priedades(): array
+    {
+        $identificador = "cat_sat_tipo_producto_id";
+        $propiedades = array("label" => "Tipo Producto");
+        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+
+        $identificador = "codigo";
+        $propiedades = array("place_holder" => "Código");
+        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+
+        $identificador = "descripcion";
+        $propiedades = array("place_holder" => "División");
+        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+
+        return $this->keys_selects;
+    }
+
+    public function modifica(bool $header, bool $ws = false): array|stdClass
+    {
+        $r_modifica =  parent::modifica(header: false);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al generar template',data:  $r_modifica, header: $header,ws:$ws);
+        }
+
+        $this->asignar_propiedad(identificador:'cat_sat_tipo_producto_id',
+            propiedades: ["id_selected"=>$this->row_upd->cat_sat_tipo_producto_id]);
+
+        $inputs = $this->genera_inputs(keys_selects:  $this->keys_selects);
+        if(errores::$error){
+            $error = $this->errores->error(mensaje: 'Error al generar inputs',data:  $inputs);
+            print_r($error);
+            die('Error');
+        }
+
+        return $r_modifica;
+    }
+}
