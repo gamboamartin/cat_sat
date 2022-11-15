3bdf7ecad44b0571ffacea5d648c4a21c910a2db
Kevin <kevin.acuna@ivitec.com.mx>
2022-11-15 15:07:43 -0500

    v0.143.12 Se ajusta tipo de comprobante

--- controllers/controlador_cat_sat_tipo_de_comprobante.php
+++ controllers/controlador_cat_sat_tipo_de_comprobante.php
@@ -9,6 +9,7 @@
 namespace controllers;
 
 use gamboamartin\cat_sat\models\cat_sat_tipo_de_comprobante;
+use gamboamartin\errores\errores;
 use gamboamartin\system\links_menu;
 use gamboamartin\system\system;
 use gamboamartin\template_1\html;
@@ -18,16 +19,95 @@ use stdClass;
 
 class controlador_cat_sat_tipo_de_comprobante extends system {
 
+    public array $keys_selects = array();
+
     public function __construct(PDO $link, stdClass $paths_conf = new stdClass()){
 
         $modelo = new cat_sat_tipo_de_comprobante(link: $link);
         $html_base = new html();
         $html = new cat_sat_tipo_de_comprobante_html(html: $html_base);
         $obj_link = new links_menu(link: $link, registro_id: $this->registro_id);
-        parent::__construct(html:$html,link: $link,modelo:  $modelo,obj_link: $obj_link, paths_conf: $paths_conf);
+
+        $columns["cat_sat_tipo_de_comprobante_id"]["titulo"] = "Id";
+        $columns["cat_sat_tipo_de_comprobante_codigo"]["titulo"] = "Código";
+        $columns["cat_sat_tipo_de_comprobante_descripcion"]["titulo"] = "Tipo Comprobante";
+
+        $filtro = array("cat_sat_tipo_de_comprobante.id","cat_sat_tipo_de_comprobante.codigo",
+            "cat_sat_tipo_de_comprobante.descripcion");
+
+        $datatables = new stdClass();
+        $datatables->columns = $columns;
+        $datatables->filtro = $filtro;
+
+        parent::__construct(html:$html, link: $link,modelo:  $modelo, obj_link: $obj_link, datatables: $datatables,
+            paths_conf: $paths_conf);
 
         $this->titulo_lista = 'Tipos de Comprobante';
 
+        $propiedades = $this->inicializa_propiedades();
+        if(errores::$error){
+            $error = $this->errores->error(mensaje: 'Error al inicializar propiedades',data:  $propiedades);
+            print_r($error);
+            die('Error');
+        }
+    }
+
+    public function alta(bool $header, bool $ws = false): array|string
+    {
+        $r_alta =  parent::alta(header: false);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al generar template',data:  $r_alta, header: $header,ws:$ws);
+        }
+
+        $inputs = $this->genera_inputs(keys_selects:  $this->keys_selects);
+        if(errores::$error){
+            $error = $this->errores->error(mensaje: 'Error al generar inputs',data:  $inputs);
+            print_r($error);
+            die('Error');
+        }
+
+        return $r_alta;
+    }
+
+    public function asignar_propiedad(string $identificador, mixed $propiedades)
+    {
+        if (!array_key_exists($identificador,$this->keys_selects)){
+            $this->keys_selects[$identificador] = new stdClass();
+        }
+
+        foreach ($propiedades as $key => $value){
+            $this->keys_selects[$identificador]->$key = $value;
+        }
+    }
+
+    private function inicializa_propiedades(): array
+    {
+        $identificador = "codigo";
+        $propiedades = array("place_holder" => "Código", "cols" => 4);
+        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+
+        $identificador = "descripcion";
+        $propiedades = array("place_holder" => "Tipo de Comprobante", "cols" => 8);
+        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+
+        return $this->keys_selects;
+    }
+
+    public function modifica(bool $header, bool $ws = false): array|stdClass
+    {
+        $r_modifica =  parent::modifica(header: false);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al generar template',data:  $r_modifica, header: $header,ws:$ws);
+        }
+
+        $inputs = $this->genera_inputs(keys_selects:  $this->keys_selects);
+        if(errores::$error){
+            $error = $this->errores->error(mensaje: 'Error al generar inputs',data:  $inputs);
+            print_r($error);
+            die('Error');
+        }
+
+        return $r_modifica;
     }
 
 
