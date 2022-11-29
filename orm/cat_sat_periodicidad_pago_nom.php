c30ea8a7a5e376be4396379c137a8328e2937b2c
mgamboa <gamboa.martin@gmail.com>
2022-11-29 13:03:30 -0600

    v0.168.13 Se integra periodicidad pago

--- orm/cat_sat_periodicidad_pago_nom.php
+++ orm/cat_sat_periodicidad_pago_nom.php
@@ -1,15 +1,20 @@
 <?php
 namespace gamboamartin\cat_sat\models;
-use base\orm\modelo;
+use base\orm\_modelo_parent;
 use PDO;
 
-class cat_sat_periodicidad_pago_nom extends modelo{
+class cat_sat_periodicidad_pago_nom extends _modelo_parent{
     public function __construct(PDO $link){
         $tabla = 'cat_sat_periodicidad_pago_nom';
         $columnas = array($tabla=>false);
         $campos_obligatorios[] = 'descripcion';
 
+        $tipo_campos['codigo'] = 'cod_int_0_2_numbers';
+        $tipo_campos['n_dias'] = 'id';
+
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
-            columnas: $columnas);
+            columnas: $columnas, tipo_campos: $tipo_campos);
+
+        $this->NAMESPACE = __NAMESPACE__;
     }
 }
--- templates/inputs/cat_sat_periodicidad_pago_nom/alta.php
+++ templates/inputs/cat_sat_periodicidad_pago_nom/alta.php
@@ -1,10 +1,7 @@
 <?php /** @var gamboamartin\cat_sat\controllers\controlador_cat_sat_grupo_producto $controlador  controlador en ejecucion */ ?>
 <?php use config\views; ?>
 <?php echo $controlador->inputs->codigo; ?>
-<?php echo $controlador->inputs->codigo_bis; ?>
 <?php echo $controlador->inputs->descripcion; ?>
-<?php echo $controlador->inputs->descripcion_select; ?>
-<?php echo $controlador->inputs->alias; ?>
 
 <?php echo $controlador->inputs->n_dias; ?>
 
