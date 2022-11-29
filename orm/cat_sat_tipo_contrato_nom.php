af0ec3b64142700fb9318dbf56f14892404b811f
javier <javier.flores@ivitec.com.mx>
2022-11-29 12:38:42 -0500

    v0.165.13 se integra regex 2 numeros

--- orm/cat_sat_tipo_contrato_nom.php
+++ orm/cat_sat_tipo_contrato_nom.php
@@ -1,15 +1,24 @@
 <?php
 namespace gamboamartin\cat_sat\models;
-use base\orm\modelo;
+use base\orm\_modelo_parent;
 use PDO;
 
-class cat_sat_tipo_contrato_nom  extends modelo{
+class cat_sat_tipo_contrato_nom  extends _modelo_parent{
     public function __construct(PDO $link){
         $tabla = 'cat_sat_tipo_contrato_nom';
         $columnas = array($tabla=>false);
         $campos_obligatorios[] = 'descripcion';
+        $campos_obligatorios[] = 'descripcion_select';
+
+        $tipo_campos['codigo'] = 'cod_int_0_2_numbers';
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
-            columnas: $columnas);
+            columnas: $columnas, tipo_campos: $tipo_campos);
+
+
+        $this->NAMESPACE = __NAMESPACE__;
     }
+
+
+
 }
