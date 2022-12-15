191310bf2364cf2749712432138d905649554b6c
mgamboa <gamboa.martin@gmail.com>
2022-12-15 16:41:15 -0600

    v3.4.0 Se ajusta disabled

--- templates/directivas/cat_sat_regimen_fiscal_html.php
+++ templates/directivas/cat_sat_regimen_fiscal_html.php
@@ -14,8 +14,10 @@ class cat_sat_regimen_fiscal_html extends html_controler {
      * Genera un select tipo regimen fiscal
      * @param int $cols Numero de columnas css
      * @param bool $con_registros si no con registros integra un select vacio
-     * @param int $id_selected identificador para selected
+     * @param int|null $id_selected identificador para selected
      * @param PDO $link conexion a la base de datos
+     * @param bool $disabled
+     * @param bool $required
      * @return array|string
      * @version 0.54.5
      * @verfuncion 0.1.0
@@ -23,7 +25,7 @@ class cat_sat_regimen_fiscal_html extends html_controler {
      * @author mgamboa
      */
     public function select_cat_sat_regimen_fiscal_id(int $cols, bool $con_registros, int|null $id_selected, PDO $link,
-                                                     bool $required = false): array|string
+                                                     bool $disabled = false, bool $required = false): array|string
     {
         $valida = (new directivas(html:$this->html_base))->valida_cols(cols:$cols);
         if(errores::$error){
@@ -36,8 +38,8 @@ class cat_sat_regimen_fiscal_html extends html_controler {
         
         $modelo = new cat_sat_regimen_fiscal($link);
 
-        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected,
-            modelo: $modelo, label: 'Regimen fiscal', required: $required);
+        $select = $this->select_catalogo(cols: $cols, con_registros: $con_registros, id_selected: $id_selected,
+            modelo: $modelo, disabled: $disabled, label: 'Regimen fiscal', required: $required);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar select', data: $select);
         }
