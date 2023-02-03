fd6563079303c391130119e5c435c883005f080e
Kevin <kevin.acuna@ivitec.com.mx>
2023-02-03 15:17:55 -0500

    v7.7.0 Se ajusta acciones en forma pago

--- controllers/controlador_cat_sat_forma_pago.php
+++ controllers/controlador_cat_sat_forma_pago.php
@@ -8,75 +8,112 @@
  */
 namespace gamboamartin\cat_sat\controllers;
 
+use base\controller\controler;
 use gamboamartin\cat_sat\models\cat_sat_forma_pago;
 use gamboamartin\errores\errores;
 use gamboamartin\system\links_menu;
-use gamboamartin\system\system;
 use gamboamartin\template\html;
 use html\cat_sat_forma_pago_html;
 use PDO;
 use stdClass;
 
-class controlador_cat_sat_forma_pago extends _cat_sat {
-
-    public array $keys_selects = array();
+class controlador_cat_sat_forma_pago extends _cat_sat_base {
 
     public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(),
                                 stdClass $paths_conf = new stdClass()){
         $modelo = new cat_sat_forma_pago(link: $link);
-        $html = new cat_sat_forma_pago_html(html: $html);
+        $html_ = new cat_sat_forma_pago_html(html: $html);
         $obj_link = new links_menu(link: $link, registro_id: $this->registro_id);
 
+        $datatables = $this->init_datatable();
+        if (errores::$error) {
+            $error = $this->errores->error(mensaje: 'Error al inicializar datatable', data: $datatables);
+            print_r($error);
+            die('Error');
+        }
+
+        parent::__construct(html: $html_, link: $link, modelo: $modelo, obj_link: $obj_link, datatables: $datatables,
+            paths_conf: $paths_conf);
+
+        $configuraciones = $this->init_configuraciones();
+        if (errores::$error) {
+            $error = $this->errores->error(mensaje: 'Error al inicializar configuraciones', data: $configuraciones);
+            print_r($error);
+            die('Error');
+        }
+    }
+
+    protected function campos_view(): array
+    {
+        $keys = new stdClass();
+        $keys->inputs = array('codigo', 'descripcion');
+        $keys->selects = array();
+
+        $init_data = array();
+
+        $campos_view = $this->campos_view_base(init_data: $init_data, keys: $keys);
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al inicializar campo view', data: $campos_view);
+        }
+
+        return $campos_view;
+    }
+
+    private function init_configuraciones(): controler
+    {
+        $this->seccion_titulo = 'SAT Formas de Pago';
+        $this->titulo_lista = 'Registro de Formas de Pago';
+
+        $this->lista_get_data = true;
+
+        return $this;
+    }
+
+    private function init_datatable(): stdClass
+    {
         $columns["cat_sat_forma_pago_id"]["titulo"] = "Id";
         $columns["cat_sat_forma_pago_codigo"]["titulo"] = "Código";
         $columns["cat_sat_forma_pago_descripcion"]["titulo"] = "Forma Pago";
 
-        $filtro = array("cat_sat_forma_pago.id","cat_sat_forma_pago.codigo", "cat_sat_forma_pago.descripcion");
+        $filtro = array("cat_sat_forma_pago.id","cat_sat_forma_pago.codigo","cat_sat_forma_pago.descripcion");
 
         $datatables = new stdClass();
         $datatables->columns = $columns;
         $datatables->filtro = $filtro;
 
-        parent::__construct(html:$html, link: $link,modelo:  $modelo, obj_link: $obj_link, datatables: $datatables,
-            paths_conf: $paths_conf);
-
-        $this->titulo_lista = 'Formas de Pago';
-
-        $propiedades = $this->inicializa_propiedades();
-        if(errores::$error){
-            $error = $this->errores->error(mensaje: 'Error al inicializar propiedades',data:  $propiedades);
-            print_r($error);
-            die('Error');
-        }
-        $this->lista_get_data = true;
+        return $datatables;
     }
 
-
-
-    public function asignar_propiedad(string $identificador, mixed $propiedades)
+    protected function key_selects_txt(array $keys_selects): array
     {
-        if (!array_key_exists($identificador,$this->keys_selects)){
-            $this->keys_selects[$identificador] = new stdClass();
+        $keys_selects = (new \base\controller\init())->key_select_txt(cols: 4, key: 'codigo',
+            keys_selects: $keys_selects, place_holder: 'Código');
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
         }
 
-        foreach ($propiedades as $key => $value){
-            $this->keys_selects[$identificador]->$key = $value;
+        $keys_selects = (new \base\controller\init())->key_select_txt(cols: 8, key: 'descripcion',
+            keys_selects: $keys_selects, place_holder: 'Forma Pago');
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
         }
+
+        return $keys_selects;
     }
 
-    private function inicializa_propiedades(): array
+    public function modifica(bool $header, bool $ws = false): array|stdClass
     {
-        $identificador = "codigo";
-        $propiedades = array("place_holder" => "Código", "cols" => 4);
-        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+        $r_modifica = $this->init_modifica();
+        if (errores::$error) {
+            return $this->retorno_error(
+                mensaje: 'Error al generar salida de template', data: $r_modifica, header: $header, ws: $ws);
+        }
 
-        $identificador = "descripcion";
-        $propiedades = array("place_holder" => "Form Pago", "cols" => 8);
-        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+        $base = $this->base_upd(keys_selects: array(), params: array(), params_ajustados: array());
+        if (errores::$error) {
+            return $this->retorno_error(mensaje: 'Error al integrar base', data: $base, header: $header, ws: $ws);
+        }
 
-        return $this->keys_selects;
+        return $r_modifica;
     }
-
-
-
 }
