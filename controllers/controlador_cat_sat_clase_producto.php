c5ce4f2d6156071a02b33ec2bbdf0ec1a6bfba5c
mgamboa <gamboa.martin@gmail.com>
2023-03-03 13:04:38 -0600

    v7.44.2 FIX productos

--- controllers/controlador_cat_sat_clase_producto.php
+++ controllers/controlador_cat_sat_clase_producto.php
@@ -50,6 +50,8 @@ class controlador_cat_sat_clase_producto extends _cat_sat_productos {
 
         $this->parents_verifica[] = new cat_sat_grupo_producto(link: $this->link);
         $this->verifica_parents_alta = true;
+
+        $this->childrens_data['cat_sat_producto']['title'] = 'Producto';
     }
 
 
