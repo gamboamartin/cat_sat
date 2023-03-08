15383b56dbc9d80117d7d6f3fa674985328e97a7
mgamboa <gamboa.martin@gmail.com>
2023-03-08 10:23:00 -0600

    v7.53.5 Init mp

--- orm/cat_sat_metodo_pago.php
+++ orm/cat_sat_metodo_pago.php
@@ -1,6 +1,8 @@
 <?php
 namespace gamboamartin\cat_sat\models;
+use base\orm\_defaults;
 use base\orm\_modelo_parent;
+use gamboamartin\errores\errores;
 use PDO;
 
 class cat_sat_metodo_pago extends _modelo_parent {
@@ -18,5 +20,19 @@ class cat_sat_metodo_pago extends _modelo_parent {
         $this->NAMESPACE = __NAMESPACE__;
 
         $this->etiqueta = 'Metodo de Pago';
+
+        if(!isset($_SESSION['init'][$tabla])) {
+            $catalago = array();
+            $catalago[] = array('codigo' => 'PPD', 'descripcion' => 'Pago en parcialidades o diferido');
+            $catalago[] = array('codigo' => 'PUE', 'descripcion' => 'Pago en una sola exhibición');
+
+            $r_alta_bd = (new _defaults())->alta_defaults(catalago: $catalago, entidad: $this);
+            if (errores::$error) {
+                $error = $this->error->error(mensaje: 'Error al insertar', data: $r_alta_bd);
+                print_r($error);
+                exit;
+            }
+            $_SESSION['init'][$tabla] = true;
+        }
     }
 }
