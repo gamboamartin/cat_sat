b5ee315ae2c55c92a588bffdee9e7e27d93268a5
mgamboa <gamboa.martin@gmail.com>
2023-07-05 16:38:58 -0600

    v8.41.0 Se integran ds in tip

--- orm/cat_sat_tipo_persona.php
+++ orm/cat_sat_tipo_persona.php
@@ -1,17 +1,21 @@
 <?php
 namespace gamboamartin\cat_sat\models;
+use base\orm\_modelo_parent_sin_codigo;
 use base\orm\modelo;
 use PDO;
 
-class cat_sat_tipo_persona extends modelo{
+class cat_sat_tipo_persona extends _modelo_parent_sin_codigo {
     public function __construct(PDO $link){
         $tabla = 'cat_sat_tipo_persona';
         $columnas = array($tabla=>false);
         $campos_obligatorios[] = 'descripcion';
+        $campos_obligatorios[] = 'descripcion_select';
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
             columnas: $columnas);
 
         $this->etiqueta = 'Tipo Persona';
     }
+
+
 }
