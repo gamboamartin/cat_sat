9e2dfa9d27e1b882308d3ab1bc5cee6d9ffb32ec
MauricioHdz <mauricioisraelhernandez@gmail.com>
2024-03-22 10:28:23 -0600

    v21.2.0 Se integra sobreescritura de funcion de insercion

--- orm/cat_sat_periodo.php
+++ orm/cat_sat_periodo.php
@@ -5,18 +5,16 @@ use gamboamartin\errores\errores;
 use PDO;
 
 class cat_sat_periodo extends _modelo_parent{
-    public function __construct(PDO $link, bool $aplica_transacciones_base = false){
+    public function __construct(PDO $link){
         $tabla = 'cat_sat_periodo';
         $columnas = array($tabla=>false,'cat_sat_periodicidad' => $tabla);
         $campos_obligatorios[] = 'descripcion';
         $campos_obligatorios[] = 'descripcion_select';
 
         $tipo_campos = array();
-        $this->aplica_transacciones_base = $aplica_transacciones_base;
 
-
-        parent::__construct(link: $link, tabla: $tabla, aplica_transacciones_base: $aplica_transacciones_base,
-            campos_obligatorios: $campos_obligatorios, columnas: $columnas, tipo_campos: $tipo_campos);
+        parent::__construct(link: $link, tabla: $tabla, campos_obligatorios: $campos_obligatorios, columnas: $columnas,
+            tipo_campos: $tipo_campos);
 
         $this->NAMESPACE = __NAMESPACE__;
 
@@ -26,14 +24,24 @@ class cat_sat_periodo extends _modelo_parent{
 
     public function alta_bd(array $keys_integra_ds = array('codigo', 'descripcion')): array|\stdClass
     {
-        $keys = array('codigo','descripcion');
-        $valida = $this->validacion->valida_existencia_keys(keys: $keys,registro:  $this->registro);
-        if(errores::$error){
-            return $this->error->error(mensaje:'Error al validar registro',data:  $valida);
-
+        $r_periodicidad = (new cat_sat_periodicidad(link: $this->link))->registro(
+            registro_id: $this->registro['cat_sat_periodicidad_id']);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al insertar com tipo periodicidad', data: $r_periodicidad);
         }
 
-        $this->registro['id'] = $this->registro['codigo'];
+        if(!isset($this->registro['codigo'])){
+            $this->registro['codigo'] = $this->registro['fecha_inicio'].$this->registro['fecha_fin'].rand();
+        }
+
+        if(!isset($this->registro['descripcion'])){
+            $this->registro['descripcion'] = $r_periodicidad['cat_sat_periodicidad_descripcion']." - ".
+                $this->registro['fecha_inicio']." al ".$this->registro['fecha_fin'];
+        }
+
+        if(!isset($this->registro['descripcion_select'])){
+            $this->registro['descripcion_select'] = $this->registro['descripcion'];
+        }
 
         $r_alta_bd = parent::alta_bd(keys_integra_ds: $keys_integra_ds);
         if(errores::$error){
