a8b44b46c4129f236da07f642c88cc84d8833ac5
mgamboa <gamboa.martin@gmail.com>
2024-07-15 10:14:51 -0600

    v24.20.0 Se integra actividad label

--- templates/directivas/cat_sat_actividad_economica_html.php
+++ templates/directivas/cat_sat_actividad_economica_html.php
@@ -8,11 +8,13 @@ use PDO;
 
 class cat_sat_actividad_economica_html extends html_controler {
 
-    public function select_cat_sat_actividad_economica_id(int $cols, bool $con_registros, int $id_selected, PDO $link): array|string
+    public function select_cat_sat_actividad_economica_id(int $cols, bool $con_registros, int $id_selected, PDO $link,
+                                                          string $label = ''): array|string
     {
         $modelo = new cat_sat_actividad_economica($link);
 
-        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected, modelo: $modelo);
+        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected,
+            modelo: $modelo,label: $label);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar select', data: $select);
         }
