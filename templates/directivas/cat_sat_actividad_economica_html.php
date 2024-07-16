b6a1396c380179b13fcf67f9739ffdd1b0f25ee8
mgamboa <gamboa.martin@gmail.com>
2024-07-16 09:14:17 -0600

    v24.23.0 Se integra id null repcepcion

--- templates/directivas/cat_sat_actividad_economica_html.php
+++ templates/directivas/cat_sat_actividad_economica_html.php
@@ -8,9 +8,12 @@ use PDO;
 
 class cat_sat_actividad_economica_html extends html_controler {
 
-    public function select_cat_sat_actividad_economica_id(int $cols, bool $con_registros, int $id_selected, PDO $link,
-                                                          string $label = ''): array|string
+    public function select_cat_sat_actividad_economica_id(int $cols, bool $con_registros, int|null $id_selected,
+                                                          PDO $link, string $label = ''): array|string
     {
+        if(is_null($id_selected)){
+            $id_selected = -1;
+        }
         $modelo = new cat_sat_actividad_economica($link);
 
         $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected,
