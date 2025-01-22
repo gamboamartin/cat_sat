a86f9e12f6ca1965ec777e51a807fd920d797437
MauricioHdz <mauricioisraelhernandez@gmail.com>
2025-01-21 18:39:11 -0600

    v25.1.0 Se integra elemento label

--- templates/directivas/cat_sat_obj_imp_html.php
+++ templates/directivas/cat_sat_obj_imp_html.php
@@ -130,12 +130,12 @@ class cat_sat_obj_imp_html extends html_controler {
         return $selects;
     }
 
-    public function select_cat_sat_obj_imp_id(int $cols, bool $con_registros, int $id_selected, PDO $link): array|string
+    public function select_cat_sat_obj_imp_id(int $cols, bool $con_registros, int $id_selected,string $label, PDO $link): array|string
     {
         $modelo = new cat_sat_obj_imp(link: $link);
 
         $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected,
-            modelo: $modelo,label: 'OBJ',required: true);
+            modelo: $modelo,label: $label,required: true);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar select', data: $select);
         }
